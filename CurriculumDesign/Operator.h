a6f04122fcec5d5554c58ec1a41dd07e22bff5a0
Lancer <Lancer@LANCER>
2016-12-27 18:10:38 +0800

    Operator的小修改

--- CurriculumDesign/Operator.h
+++ CurriculumDesign/Operator.h
@@ -4,10 +4,12 @@
 #include"Manager.h"
 class Operator {
 public:
-	friend class Person;
 	void setpassword(Person &p, int x) { p.password = x; }
 	void createMember(char *inputname, int inputid, int inputpassword);
 	void createManager(char *inputname, int inputid, int inputpassword);
 	bool certid(int inputid);   //判断id或密码存在性 返回ture存在
 	bool certpw(int inputpw);
+	virtual void checkid() = 0;  //查询id信息，此为纯虚函数 需要重新定义
+
+
 };
